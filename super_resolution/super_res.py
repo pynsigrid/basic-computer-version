6bc0adb3bf19b32152490d058823b35e4a672c54
Yining Pan <44635829+pynsigrid@users.noreply.github.com>
2021-02-20 10:35:49 +0800

    add sr for batch
    对文件夹内的图片批量处理

---
+++ super_resolution/super_res.py
@@ -0,0 +1,51 @@
+# USAGE
+# python super_res_video.py --model models/ESPCN_x4.pb
+
+# import the necessary packages
+from imutils.video import VideoStream
+import argparse
+import imutils
+import time
+import cv2
+from cv2 import dnn_superres
+import os
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-m", "--model", required=True,
+	help="path to super resolution model")
+ap.add_argument("-i", "--image", required=True,
+	help="path to image")
+args = vars(ap.parse_args())
+
+# extract the model name and model scale from the file path
+modelName = args["model"].split("/")[-1].split("_")[0].lower()
+modelScale = args["model"].split("_x")[-1]
+modelScale = int(modelScale[:modelScale.find(".")])
+
+# initialize OpenCV's super resolution DNN object, load the super
+# resolution model from disk, and set the model name and scale
+print("[INFO] loading super resolution model: {}".format(
+	args["model"]))
+print("[INFO] model name: {}".format(modelName))
+print("[INFO] model scale: {}".format(modelScale))
+sr = dnn_superres.DnnSuperResImpl_create()
+sr.readModel(args["model"])
+sr.setModel(modelName, modelScale)
+
+time.sleep(2.0)
+
+image = cv2.imread(args["image"],1)
+print(f'shape = {image.shape}')
+upscaled = sr.upsample(image)
+bicubic = cv2.resize(image,
+    (upscaled.shape[1], upscaled.shape[0]),
+    interpolation=cv2.INTER_CUBIC)
+
+# show the original frame, bicubic interpolation frame, and super
+# resolution frame
+cv2.imshow("Original", image)
+cv2.imshow("Bicubic", bicubic)
+cv2.imshow("Super Resolution", upscaled)
+cv2.imwrite("butterfly_SR.png", upscaled)
+cv2.waitKey(0)
